faae8583055116c364de09015547c90a7753d71e
Peng Rong <rongpeng@live.com>
2018-05-29 20:22:44 +0800

    Init project

---
+++ console-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
