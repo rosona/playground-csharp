870abf487cbf35dbda6992d2a6ab4107389332fe
Peng Rong <rongpeng@live.com>
2018-05-30 09:14:23 +0800

    Remove test parse

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ev
 dotnet restore -s "https://nuget.cdn.azure.cn/v3/index.json" -s "https://api.nuget.org/v3/index.json" "playground-csharp.sln"
-dotnet test
 dotnet build "console-test/console-test.csproj"
